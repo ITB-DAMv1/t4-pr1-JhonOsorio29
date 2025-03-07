ea276d73a398cd0be35b16a37d7bcc6730c36a46
JhonOsorio29 <jhon.osorio.7e5@itb.cat>
2025-03-07 23:00:36 +0100

    Iniciacion pagina principal

---
+++ t4-pr1-JhonOsorio29/t4-pr1-JhonOsorio29/Pages/Simulation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace t4_pr1_JhonOsorio29.Pages
+{
+    public class SimulationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- t4-pr1-JhonOsorio29/t4-pr1-JhonOsorio29/obj/Debug/net8.0/t4-pr1-JhonOsorio29.AssemblyInfo.cs
+++ t4-pr1-JhonOsorio29/t4-pr1-JhonOsorio29/obj/Debug/net8.0/t4-pr1-JhonOsorio29.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("t4-pr1-JhonOsorio29")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab451b1ce1de4324138892352526b1c3dbde9b97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e81edea202c6b17d54394c93d165dc9e4106b9cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("t4-pr1-JhonOsorio29")]
 [assembly: System.Reflection.AssemblyTitleAttribute("t4-pr1-JhonOsorio29")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
