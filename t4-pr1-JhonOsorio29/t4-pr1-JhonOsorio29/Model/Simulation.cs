0503bf5c12d2a77079f38abc3fbda6d18d70e76d
JhonOsorio29 <jhon.osorio.7e5@itb.cat>
2025-03-08 00:28:17 +0100

    Guardado, por si peta

--- t4-pr1-JhonOsorio29/t4-pr1-JhonOsorio29/Model/Simulation.cs
+++ t4-pr1-JhonOsorio29/t4-pr1-JhonOsorio29/Model/Simulation.cs
@@ -1,6 +1,38 @@
-﻿namespace t4_pr1_JhonOsorio29.Model
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+namespace t4_pr1_JhonOsorio29.Model
 {
     public class Simulation
     {
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        [Required]
+        public string Tipe { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "el valor debe de ser mayor de 0")]
+        public double Parameter { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "el valor debe de ser de 0 a 3")]
+        public double Rati { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El costo debe de ser mayor a 0")]
+        public double CostKWh { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe de ser mayor a 0")]
+        public double PriceKWh { get; set; }
+
+        public double EnergyGenerated { get; set;}
+        public double TotalCostKWh => EnergyGenerated * CostKWh;
+        public double TotalPriceKWh => EnergyGenerated * PriceKWh;
+
+        public void calcularEnergia()
+        {
+            switch (Tipo)
+        }
+
     }
 }
--- t4-pr1-JhonOsorio29/t4-pr1-JhonOsorio29/Pages/Simulation.cshtml.cs
+++ t4-pr1-JhonOsorio29/t4-pr1-JhonOsorio29/Pages/Simulation.cshtml.cs
@@ -1,12 +1,51 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics;
+using System.Globalization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Diagnostics;
+using System.Globalization;
+using FileWorking = System.IO;
+using t4_pr1_JhonOsorio29.Model;
 
 namespace t4_pr1_JhonOsorio29.Pages
 {
     public class SimulationModel : PageModel
     {
-        public void OnGet()
-        {
-        }
-    }
+		public string FileErrorMessage;
+		public List<SimulationModel> Products { get; set; } = new List<SimulationModel> { };
+		public void OnGet()
+		{
+			string filePath = @"ModelData\product.txt";
+			if (FileWorking.File.Exists(filePath))
+			{
+				string[] lines = FileWorking.File.ReadAllLines(filePath);
+				foreach (string line in lines)
+				{
+
+					string[] parts = line.Split('|');
+					if (parts.Length == 4)
+					{
+						Simulation product = new Simulation();
+						Simulation.Id = int.Parse(parts[0]);
+						Simulation.Name = parts[1];
+						product.Amount = int.Parse(parts[2]);
+						product.Price = decimal.Parse(parts[3], CultureInfo.InvariantCulture);
+						Products.Add(product);
+					}
+					else
+					{
+						FileErrorMessage = "Error de carrega dels atributs d'un producte";
+					}
+				}
+			}
+			else
+			{
+				FileErrorMessage = "Error de carrega de dades";
+			}
+			Debug.WriteLine(Path.GetFullPath(filePath));
+		}
+	}
+
 }
